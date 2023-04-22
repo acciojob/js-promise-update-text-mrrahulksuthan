07951ea36bec41cbde8bc182526b5d9f6872e167
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-04-22 09:45:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateOutput() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const message = "Hello, world!";
+      resolve(message);
+    }, 1000);
+  }).then((message) => {
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = message;
+  });
+}
